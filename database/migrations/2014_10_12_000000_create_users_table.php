cd6813a446ecfeee0c702405d96acf1e002c5084
Harsh0726 <harshanineranjana@gmail.com>
2024-02-04 23:41:54 +0530

    create user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('contact_number')->nullable();
+            $table->string('books')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->timestamp('borrowed_date')->useCurrent();
+            $table->timestamp('returned_date')->nullable();
             $table->timestamps();
         });
     }
