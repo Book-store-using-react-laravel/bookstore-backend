3ebb29060454a5299baf56a00c97498c70cc3250
Harsh0726 <harshanineranjana@gmail.com>
2024-02-06 23:47:35 +0530

    created table for members

---
+++ database/migrations/2024_02_06_180540_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('contact_number');
+            $table->unsignedBigInteger('member_id')->unique();
+            $table->json('books')->nullable();
+            $table->timestamp('borrowed_date')->useCurrent();
+            $table->timestamp('returned_date')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
